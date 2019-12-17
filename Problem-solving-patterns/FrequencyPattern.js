ecb2b8ad64903669f6a9ff113d20773e824ea152
Mateen Kazia <mateencode@gmail.com>
2019-12-17 16:00:47 -0600

    frequecy pattern completed

--- Problem-solving-patterns/FrequencyPattern.js
+++ Problem-solving-patterns/FrequencyPattern.js
@@ -1,5 +1,25 @@
-same = () => {};
+same = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-console.log([1, 2, 3], [4, 1, 9]); //true
-console.log([1, 2, 3], [1, 9]); //false
-console.log([1, 2, 1], [4, 4, 1]); // false (must be same frequency)
+  let count1 = {};
+  let count2 = {};
+
+  for (let val of arr1) {
+    count1[val] = (count1[val] || 0) + 1;
+  }
+  for (let val of arr2) {
+    count2[val] = (count2[val] || 0) + 1;
+  }
+
+  for (let key in count1) {
+    if (!(key ** 2 in count2)) return false;
+    if (count2[key ** 2] !== count1[key]) return false;
+  }
+  return true;
+};
+
+console.log(same([1, 2, 3], [4, 1, 9])); //true
+console.log(same([1, 2, 3], [1, 9])); //false
+console.log(same([1, 2, 1], [4, 4, 1])); // false (must be same frequency)
