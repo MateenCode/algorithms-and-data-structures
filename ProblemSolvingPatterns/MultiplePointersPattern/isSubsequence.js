1f01136f9efaac8d36563e10f6f138861c727d36
Mateen Kazia <mateencode@gmail.com>
2019-12-24 12:48:59 -0600

    searching algo complete

--- ProblemSolvingPatterns/MultiplePointersPattern/isSubsequence.js
+++ ProblemSolvingPatterns/MultiplePointersPattern/isSubsequence.js
@@ -6,11 +6,16 @@
 function isSubsequence(str1, str2) {
   let i = 0;
   let j = 0;
+  // while j is less then str2s length
   while (j < str2.length) {
+    // if str2[j] is equal to str1[i] then increment i
     if (str2[j] === str1[i]) i++;
+    // if i is equal to str1 length then return true
     if (i === str1.length) return true;
+    // everything else j++
     j++;
   }
+  // if anything else return false
   return false;
 }
 console.log(isSubsequence("hello", "hello world")); // true
--- ProblemSolvingPatterns/SlidingWindowPattern/minSubArrayLen.js
+++ ProblemSolvingPatterns/SlidingWindowPattern/minSubArrayLen.js
@@ -5,8 +5,33 @@
 // of which the sum is greater than or equal to the integer passed to the function.
 // if there isn't one, return 0 instead.
 
-function maxSubarrayLen() {
-  // add whatever parameters you deem necessary - good luck!
+function maxSubarrayLen(nums, sum) {
+  let total = 0;
+  let start = 0;
+  let end = 0;
+  let minLen = Infinity;
+
+  while (start < nums.length) {
+    // if current window doesn't add up to the given sum then
+    // move the window to right
+    if (total < sum && end < nums.length) {
+      total += nums[end];
+      end++;
+    }
+    // if current window adds up to at least the sum given then
+    // we can shrink the window
+    else if (total >= sum) {
+      minLen = Math.min(minLen, end - start);
+      total -= nums[start];
+      start++;
+    }
+    // current total less than required total but we reach the end, need this or else we'll be in an infinite loop
+    else {
+      break;
+    }
+  }
+
+  return minLen === Infinity ? 0 : minLen;
 }
 
 console.log(maxSubarrayLen([2, 3, 1, 2, 4, 3], 7)); // 2 -> because [4,3] is the smallest subarray
