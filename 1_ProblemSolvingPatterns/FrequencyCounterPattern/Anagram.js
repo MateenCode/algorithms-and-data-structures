b0a156fbfeca45b68234a1dce7d7fbb174e60985
Mateen Kazia <mateencode@gmail.com>
2019-12-25 15:28:51 -0600

    anagram pratice

--- 1_ProblemSolvingPatterns/FrequencyCounterPattern/Anagram.js
+++ 1_ProblemSolvingPatterns/FrequencyCounterPattern/Anagram.js
@@ -3,16 +3,18 @@
 
 function validAnagram(first, second) {
   if (first.length !== second.length) return false;
+
   let lookup = {};
+
   for (let i = 0; i < first.length; i++) {
-    // if letter exists, increment, otherwise set to 1
     lookup[first[i]] ? lookup[first[i]]++ : (lookup[first[i]] = 1);
   }
-  // can't find letter or letter is zero then it's not an anagram
+
   for (let i = 0; i < second.length; i++) {
     if (!lookup[second[i]]) return false;
-    lookup[second[i]] -= 1;
+    lookup[second[i]]--;
   }
+
   return true;
 }
 
