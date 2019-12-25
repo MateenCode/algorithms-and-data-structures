816c99ff36aabe36e32751e32fe6b2cf511ed580
Mateen Kazia <mateencode@gmail.com>
2019-12-25 12:07:55 -0600

    basic sorts completed

--- SortingAlgorithms/Basic/selectSort.js
+++ SortingAlgorithms/Basic/selectSort.js
@@ -8,13 +8,12 @@ function selectSort(arr) {
   for (let i = 0; i < arr.length; i++) {
     let lowest = i;
     for (let j = i + 1; j < arr.length; j++) {
-      if (arr[lowest] > arr[j]) {
+      if (arr[j] < arr[lowest]) {
         lowest = j;
       }
     }
     if (i !== lowest) swap(arr, i, lowest);
   }
-
   return arr;
 }
 
