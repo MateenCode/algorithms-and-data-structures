67dbc79d70d26e4b1a690bea91e05dc455b86e09
Mateen Kazia <mateencode@gmail.com>
2019-12-29 16:19:03 -0600

    search completed

--- 4_SortingAlgorithms/Basic/bubbleSort.js
+++ 4_SortingAlgorithms/Basic/bubbleSort.js
@@ -2,17 +2,16 @@
 function bubbleSort(arr) {
   // actually swaps the values
   const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    [arr[idx1], arr[idx2]], [arr[idx2], arr[idx1]];
   };
 
   for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < i - 1; j++) {
+    for (let j = 0; j < 0; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
     }
   }
-  return arr;
 }
 
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
