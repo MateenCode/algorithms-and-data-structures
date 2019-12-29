0ff76f8aa04cc596226019119f1b2a3e6a6c2e9d
Mateen Kazia <mateencode@gmail.com>
2019-12-29 16:38:24 -0600

    bubble sort completed

--- 4_SortingAlgorithms/Basic/bubbleSort.js
+++ 4_SortingAlgorithms/Basic/bubbleSort.js
@@ -1,17 +1,17 @@
 // sorts the largest values "bubble" up to the top
 function bubbleSort(arr) {
   // actually swaps the values
-  const swap = (arr, idx1, idx2) => {
-    [arr[idx1], arr[idx2]], [arr[idx2], arr[idx1]];
+  const swap = (arr, index1, index2) => {
+    [arr[index1], arr[index2]] = [arr[index2], arr[index1]];
   };
-
   for (let i = arr.length; i > 0; i--) {
-    for (let j = 0; j < 0; j++) {
+    for (let j = 0; j < i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         swap(arr, j, j + 1);
       }
     }
   }
+  return arr;
 }
 
 console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]));
